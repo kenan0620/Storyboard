0d57ed8633f78904eeaf3b06ddb9af20efab67a6
Coenen <houkenan0620@126.com>
2017-11-27 17:19:23 +0800

    171127、UISegmentedControl完成

---
+++ Storyboard/Storyboard/EightVC.h
@@ -0,0 +1,13 @@
+//
+//  EightVC.h
+//  Storyboard
+//
+//  Created by Conan on 2017/11/27.
+//  Copyright © 2017年 柯南. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EightVC : UIViewController
+
+@end
---
+++ Storyboard/Storyboard/EightVC.m
@@ -0,0 +1,43 @@
+//
+//  EightVC.m
+//  Storyboard
+//
+//  Created by Conan on 2017/11/27.
+//  Copyright © 2017年 柯南. All rights reserved.
+//
+
+#import "EightVC.h"
+
+@interface EightVC ()
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentView;
+
+@end
+
+@implementation EightVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+}
+
+- (IBAction)segmentChange:(UISegmentedControl *)sender {
+    NSLog(@"selectedSegmentIndex~%ld",sender.selectedSegmentIndex);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

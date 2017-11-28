0ee5d459d8b12374901f09974c09eae17c3f5b5e
Coenen <houkenan0620@126.com>
2017-11-28 15:50:29 +0800

    171128、UIStackView完成

---
+++ Storyboard/Storyboard/NineVC.h
@@ -0,0 +1,13 @@
+//
+//  NineVC.h
+//  Storyboard
+//
+//  Created by Conan on 2017/11/28.
+//  Copyright © 2017年 柯南. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NineVC : UIViewController
+
+@end
---
+++ Storyboard/Storyboard/TenVC.m
@@ -0,0 +1,38 @@
+//
+//  TenVC.m
+//  Storyboard
+//
+//  Created by Conan on 2017/11/28.
+//  Copyright © 2017年 柯南. All rights reserved.
+//
+
+#import "TenVC.h"
+
+@interface TenVC ()
+@property (weak, nonatomic) IBOutlet UIStackView *basisStackView;
+
+@end
+
+@implementation TenVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
